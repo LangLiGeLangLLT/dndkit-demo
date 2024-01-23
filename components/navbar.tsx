3846a2b45daef69b13aeb460667f4da608642ab7
llt <418797644@qq.com>
2024-01-23 22:52:34 +0800

    update

---
+++ components/navbar.tsx
@@ -0,0 +1,81 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+
+type Item = {
+  title: string
+  children?: Item[]
+  href?: string
+}
+
+const items: Item[] = [
+  {
+    title: 'useDraggable',
+    children: [
+      {
+        title: 'Basic Setup',
+        href: '/useDraggable/basic-setup',
+      },
+      {
+        title: 'Drag Handle',
+        href: '/useDraggable/drag-handle',
+      },
+      {
+        title: 'Press Delay',
+        href: '/useDraggable/press-delay',
+      },
+      {
+        title: 'Press Delay Or Minimum Distance',
+        href: '/useDraggable/press-delay-or-minimum-distance',
+      },
+      {
+        title: 'Minimum Distance',
+        href: '/useDraggable/minimum-distance',
+      },
+      {
+        title: 'Minimum Distance - X Axis',
+        href: '/useDraggable/minimum-distance-x-axis',
+      },
+      {
+        title: 'Minimum Distance - Y Axis',
+        href: '/useDraggable/minimum-distance-y-axis',
+      },
+    ],
+  },
+]
+
+export default function Navbar() {
+  return <NavItems items={items} />
+}
+
+function NavItems({ items }: { items: Item[] }) {
+  return (
+    <div className="-ml-4">
+      {items.map((item) => (
+        <NavItem key={item.title} item={item} />
+      ))}
+    </div>
+  )
+}
+
+function NavItem({ item }: { item: Item }) {
+  if (item.children) {
+    return (
+      <div className="ml-4">
+        <h2 className="text-2xl">{item.title}</h2>
+        <div>
+          {item.children.map((v) => (
+            <div key={v.title}>
+              <NavItem item={v} />
+            </div>
+          ))}
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <Button asChild variant="link" className="p-0">
+      <Link href={item.href!}>{item.title}</Link>
+    </Button>
+  )
+}
